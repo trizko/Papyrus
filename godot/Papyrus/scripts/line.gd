db013c85fd3de9116826b08353c8712b48ab2d11
Tony Rizko <tony@rizkocircle.com>
2023-11-05 22:19:11 -0800

    Add ability to draw multiple lines

--- godot/Papyrus/scripts/line.gd
+++ godot/Papyrus/scripts/line.gd
@@ -8,21 +8,20 @@ var end_point : Vector2
 var is_drawing : bool = false
 var line : Line2D
 
-func _ready():
-	line = $Line2D
-	line.width = 2
-
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
 				# when the mouse button is pressed down, start drawing
 				is_drawing = true
+				line = Line2D.new()
 				start_point = event.position
 				line.points = [start_point]
 			else:
 				# when the mouse button is released, stop drawing
 				is_drawing = false
+				add_child(line)
+				line = null
 	elif event is InputEventMouseMotion and is_drawing:
 		# while dragging, update the end point and draw the line
 		end_point = event.position
