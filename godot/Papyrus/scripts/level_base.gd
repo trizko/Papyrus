51f51b50a3b5a477bb1fcc433ea1fa7d270a2761
Tony Rizko <tony@rizkocircle.com>
2023-11-22 21:23:54 -0800

    Add ball position to level instantiation logic

--- godot/Papyrus/scripts/level_base.gd
+++ godot/Papyrus/scripts/level_base.gd
@@ -3,19 +3,28 @@ extends Node
 var line_scene = preload("res://scenes/line.tscn")
 var ball_scene = preload("res://scenes/object.tscn")
 var obstacle_scene = preload("res://scenes/obstacle.tscn")
+var ball_start_position = null
 var current_line_instance = null
 var Ball = null
 
 func _ready():
+	# parse level data
 	var json_as_text = FileAccess.get_file_as_string("res://levels.json")
 	var level_data = JSON.parse_string(json_as_text)
+
+	# get ball starting position and instantiate ball
+	ball_start_position = Vector2(
+		level_data["ball"]["position_x"],
+		level_data["ball"]["position_y"],
+	)
+	reset_ball(ball_start_position)
+
+	# get and instantiate all obstacles
 	for o in level_data["obstacles"]:
-		print(o)
 		var obstacle = obstacle_scene.instantiate()
 		obstacle.start_point = Vector2(o["start_point_x"], o["start_point_y"])
 		obstacle.end_point = Vector2(o["end_point_x"], o["end_point_y"])
 		add_child(obstacle)
-	reset_ball()
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -38,16 +47,16 @@ func _on_play_reload_button_toggled(button_pressed):
 		Ball.gravity_scale = 1.0
 	else:
 		Ball.queue_free()
-		reset_ball()
+		reset_ball(ball_start_position)
 
 func _on_body_entered(body):
 	if body.name == "Goal":
 		Ball.gravity_scale = 0.0
 		Ball.linear_velocity = Vector2.ZERO
 
-func reset_ball():
+func reset_ball(pos):
 	Ball = ball_scene.instantiate()
-	Ball.set_position($BallStartPosition.position)
+	Ball.set_position(pos)
 	add_child(Ball)
 	Ball.gravity_scale = 0.0
 	Ball.body_entered.connect(_on_body_entered)
