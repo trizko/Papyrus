ed7614b87104a6c4eb7b8a91f188eddd17d41d23
Tony Rizko <tony@rizkocircle.com>
2023-11-28 21:46:23 -0800

    Make prompt optional and set default for level gen post request

--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from openai import OpenAI
 from pydantic import BaseModel
+from typing import Optional
 
 load_dotenv()
 
@@ -12,7 +13,42 @@ client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 app = FastAPI()
 
 class LevelGenerationRequest(BaseModel):
-    prompt: str = "Write a simple web server in roc-lang."
+    prompt: Optional[str] = """
+I have created a game where the goal is to get a ball to a goal. A level of the
+game starts with a ball suspended in the air. At this point, the user is allowed
+to draw a specific number of lines in the scene. The lines act as platforms for
+the ball to roll on and the goal is for the player to draw the correct lines to
+get the ball to the goal. There will be obstacles in the way of the goal from
+the starting position of the ball that the player will have to draw lines
+around. The user then presses the "Play" button to turn on the gravity for the
+ball and the ball will fall on the lines and obstacles and, hopefully, get to
+the goal line. Here is an example level in JSON format:
+
+```json
+{
+  "max_lines": 2,
+  "ball": {
+    "position_x": 100.0,
+    "position_y": 50.0
+  },
+  "obstacles": [
+    {"start_point_x": 100.0, "start_point_y": 300.0, "end_point_x": 300.0, "end_point_y": 300.0},
+    {"start_point_x": 300.0, "start_point_y": 500.0, "end_point_x": 500.0, "end_point_y": 500.0},
+    {"start_point_x": 100.0, "start_point_y": 700.0, "end_point_x": 300.0, "end_point_y": 900.0},
+    {"start_point_x": 400.0, "start_point_y": 800.0, "end_point_x": 600.0, "end_point_y": 1000.0},
+    {"start_point_x": 700.0, "start_point_y": 1100.0, "end_point_x": 900.0, "end_point_y": 1300.0},
+    {"start_point_x": 200.0, "start_point_y": 1400.0, "end_point_x": 400.0, "end_point_y": 1600.0},
+    {"start_point_x": 500.0, "start_point_y": 1500.0, "end_point_x": 700.0, "end_point_y": 1700.0},
+  ],
+  "goal": {
+    "position_x": 950.0,
+    "position_y": 1800.0
+  }
+}
+```
+
+Responding with only JSON, generate a new level.
+    """
 
 @app.post("/generate-level/")
 async def generate_text(data: LevelGenerationRequest):
