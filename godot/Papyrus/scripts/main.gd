ac526b643b6ed363096dcc2dde6d9152009363c2
Tony Rizko <tony@rizkocircle.com>
2023-12-02 15:53:19 -0800

    Remove level editor from start menu

--- godot/Papyrus/scripts/main.gd
+++ godot/Papyrus/scripts/main.gd
@@ -1,10 +1,4 @@
 extends PanelContainer
 
-var level_scene = preload("res://scenes/level_base.tscn")
-var level_scene_instance = level_scene.instantiate()
-
-func _on_level_editor_button_pressed():
-	get_tree().root.add_child(level_scene_instance)
-
 func _on_start_button_pressed():
 	get_tree().root.add_child(level_scene_instance)
