30e0674f995628a56bfb389a4377a25dae588718
Tony Rizko <tony@rizkocircle.com>
2023-12-05 22:39:23 -0800

    Add base class for inner cache package

---
+++ server/cache/base.py
@@ -0,0 +1,9 @@
+class BaseCache:
+    def get(self, key):
+        raise NotImplementedError
+
+    def set(self, key, value, timeout=None):
+        raise NotImplementedError
+
+    def delete(self, key):
+        raise NotImplementedError
