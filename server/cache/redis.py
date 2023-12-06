ff135f2fd0d552be49df584390b2f66bb5d3a350
Tony Rizko <tony@rizkocircle.com>
2023-12-05 22:42:38 -0800

    Add implementation for redis cache

---
+++ server/cache/redis.py
@@ -0,0 +1,15 @@
+from rediscluster import RedisCluster
+from .base import BaseCache
+
+class RedisCache(BaseCache):
+    def __init__(self, redis_cluster_nodes):
+        self.client = RedisCluster(startup_nodes=redis_cluster_nodes)
+
+    def get(self, key):
+        return self.client.get(key)
+
+    def set(self, key, value, timeout=None):
+        self.client.set(key, value, ex=timeout)
+
+    def delete(self, key):
+        self.client.delete(key)
