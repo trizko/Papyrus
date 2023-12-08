27fc03d8624e5d933d1925154054c6ac0ec2a599
Tony Rizko <tony@rizkocircle.com>
2023-12-07 23:21:26 -0800

    Add script for creating new server

---
+++ infra/create-do-droplet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+doctl compute droplet create \
+    --image debian-10-x64 \
+    --size s-1vcpu-1gb \
+    --region sfo3 \
+    --ssh-keys ${DO_SSH_FINGERPRINT} \
+    papyrus-server-droplet
