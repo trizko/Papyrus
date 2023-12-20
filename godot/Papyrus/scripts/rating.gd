1e99aed47be1fc9ea64f95d96275af07e059453f
Tony Rizko <tony@rizkocircle.com>
2023-12-19 21:54:34 -0800

    Add signal and method to submit rating button

--- godot/Papyrus/scripts/rating.gd
+++ godot/Papyrus/scripts/rating.gd
@@ -2,6 +2,9 @@ extends VBoxContainer
 
 var fun_stars: Array[ShaderMaterial]
 var chal_stars: Array[ShaderMaterial]
+var num_fun_stars: int
+var num_chal_stars: int
+var level_json: String
 
 func _ready():
 	fun_stars = [
@@ -27,8 +30,14 @@ func _on_fun_stars_pressed(num):
 	reset_stars(fun_stars)
 	for i in range(num):
 		fun_stars[i].set_shader_parameter("useYellow", true)
+	num_fun_stars = num
 
 func _on_chal_stars_pressed(num):
 	reset_stars(chal_stars)
 	for i in range(num):
 		chal_stars[i].set_shader_parameter("useYellow", true)
+	num_chal_stars = num
+
+func _on_submit_button_pressed():
+	# TODO: send POST request with body to server
+	get_tree().change_scene_to_packed(GlobalEnvironment.level_scene)
