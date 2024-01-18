a090641847f437984627c0600b5b60ac84b2c994
Tony Rizko <tony@rizkocircle.com>
2024-01-17 18:01:11 -0800

    Add ip of server to environment for builds

--- godot/Papyrus/scripts/environment.gd
+++ godot/Papyrus/scripts/environment.gd
@@ -8,7 +8,8 @@ var level_json: String
 # level modifiers
 var bounciness: float = 0.0
 
-const base_url: String = "http://localhost:8000"
+# const base_url: String = "http://localhost:8000"
+const base_url: String = "http://165.232.135.113:8000"
 
 func get_route(route: String) -> String:
 	var args = {
