e826b22f7f36e037389feb0a678575921c9a3b74
Tony Rizko <tony@rizkocircle.com>
2024-01-27 23:04:41 -0800

    Update main menu script to use load instead of preload for scene changes

--- godot/Papyrus/src/ui/main.gd
+++ godot/Papyrus/src/ui/main.gd
@@ -1,12 +1,12 @@
 extends PanelContainer
 
 func _on_normal_mode_button_pressed():
-	var game_scene = preload("res://src/game.tscn")
+	var game_scene = load("res://src/game.tscn")
 	GlobalEnvironment.bounciness = 0.0
 	get_tree().change_scene_to_packed(game_scene)
 
 
 func _on_bouncy_mode_button_pressed():
-	var game_scene = preload("res://src/game.tscn")
+	var game_scene = load("res://src/game.tscn")
 	GlobalEnvironment.bounciness = 1.0
 	get_tree().change_scene_to_packed(game_scene)
