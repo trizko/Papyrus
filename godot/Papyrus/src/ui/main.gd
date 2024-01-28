4a61969eab5c7ae60e77b590583131e81159a763
Tony Rizko <tony@rizkocircle.com>
2024-01-27 23:12:11 -0800

    Refactor main menu script to only load game scene once

--- godot/Papyrus/src/ui/main.gd
+++ godot/Papyrus/src/ui/main.gd
@@ -1,12 +1,12 @@
 extends PanelContainer
 
+var game_scene: PackedScene = load("res://src/game.tscn")
+
 func _on_normal_mode_button_pressed():
-	var game_scene = load("res://src/game.tscn")
 	GlobalEnvironment.bounciness = 0.0
 	get_tree().change_scene_to_packed(game_scene)
 
 
 func _on_bouncy_mode_button_pressed():
-	var game_scene = load("res://src/game.tscn")
 	GlobalEnvironment.bounciness = 1.0
 	get_tree().change_scene_to_packed(game_scene)
