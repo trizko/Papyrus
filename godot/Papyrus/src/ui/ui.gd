3d247ca5b34caddbc38fa54209479681e9a66a28
Tony Rizko <tony@rizkocircle.com>
2024-01-29 23:38:26 -0800

    Fix back button to return to main menu screen

--- godot/Papyrus/src/ui/ui.gd
+++ godot/Papyrus/src/ui/ui.gd
@@ -4,3 +4,6 @@ signal play_reload_toggled(state: bool)
 
 func _on_play_reload_toggled(state):
 	play_reload_toggled.emit(state)
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://src/ui/main.tscn")
