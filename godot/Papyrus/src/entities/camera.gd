b0e1c7c4a855bb2a2cf8d1336e2a20d32441cfaa
Tony Rizko <tony@rizkocircle.com>
2024-02-09 22:27:35 -0800

    Add signal and camera rotation handler function

--- godot/Papyrus/src/entities/camera.gd
+++ godot/Papyrus/src/entities/camera.gd
@@ -1,3 +1,10 @@
 extends Camera2D
 
+signal rotate_finished()
+
 @onready var animation_player = %AnimationPlayer
+
+func _on_level_trigger_rotatation():
+    animation_player.play("Rotate90")
+    await animation_player.animation_finished
+    rotate_finished.emit()
