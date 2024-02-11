3c215aefaed8e69b6343ac39a2808b82bc75eb58
Tony Rizko <tony@rizkocircle.com>
2024-02-10 22:59:08 -0800

    Replace camera rotation animation with node2d animaitons

--- godot/Papyrus/src/entities/camera.gd
+++ godot/Papyrus/src/entities/camera.gd
@@ -2,9 +2,13 @@ extends Camera2D
 
 signal rotate_finished()
 
-@onready var animation_player = %AnimationPlayer
+var target_rotation = 0
+
+func _process(_delta):
+	if target_rotation > 0:
+		rotation = lerp_angle(rotation, target_rotation, 0.05)
 
 func _on_level_trigger_rotatation():
-    animation_player.play("Rotate90")
-    await animation_player.animation_finished
-    rotate_finished.emit()
+	target_rotation += PI/2
+	await get_tree().create_timer(0.5).timeout
+	rotate_finished.emit()
