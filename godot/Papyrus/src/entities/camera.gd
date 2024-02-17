365f2f8dc29e60352a6c19fcb02a4003e6e29d52
Tony Rizko <tony@rizkocircle.com>
2024-02-16 20:21:32 -0800

    Update rotation to be in the negative radian directions

--- godot/Papyrus/src/entities/camera.gd
+++ godot/Papyrus/src/entities/camera.gd
@@ -6,10 +6,10 @@ var target_rotation = 0
 
 func _process(_delta):
 	position = lerp(position, Vector2(540, 540), 0.5)
-	if target_rotation > rotation:
+	if target_rotation != rotation:
 		rotation = lerp_angle(rotation, target_rotation, 0.05)
 
 func _on_level_trigger_rotatation():
-	target_rotation += PI/2
+	target_rotation -= PI/2
 	await get_tree().create_timer(0.5).timeout
 	rotate_finished.emit()
--- godot/Papyrus/src/level/level_base.gd
+++ godot/Papyrus/src/level/level_base.gd
@@ -15,9 +15,9 @@ var Ball = null
 var rotation_index = 0
 var rotation_map = [
 	Vector2.DOWN,
-	Vector2.LEFT,
-	Vector2.UP,
 	Vector2.RIGHT,
+	Vector2.UP,
+	Vector2.LEFT,
 ]
 
 func _ready():
@@ -127,11 +127,10 @@ func _on_ui_undo_pressed():
 		line.queue_free()
 
 func _on_camera_rotate_finished():
-	Ball.gravity_scale = 1.0
-	rotation_index += 1
-	rotation_index = rotation_index % 4
+	rotation_index = (rotation_index + 1) % 4
 	PhysicsServer2D.area_set_param(
 		get_viewport().world_2d.space,
 		PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR,
 		rotation_map[rotation_index]
 	)
+	Ball.gravity_scale = 1.0
