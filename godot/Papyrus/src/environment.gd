64ba8ad292d3c10889c0ab8af8dce5759b2af3bc
Tony Rizko <tony@rizkocircle.com>
2024-02-21 22:59:49 -0800

    Add normal levels and generated levels as main menu options

--- godot/Papyrus/src/environment.gd
+++ godot/Papyrus/src/environment.gd
@@ -4,6 +4,7 @@ var level_json: String
 
 # level modifiers
 var bounciness: float = 0.0
+var generated_levels: bool = true
 
 # const base_url: String = "http://localhost:8000"
 const base_url: String = "http://165.232.135.113:8000"
--- godot/Papyrus/src/ui/main.gd
+++ godot/Papyrus/src/ui/main.gd
@@ -3,10 +3,9 @@ extends PanelContainer
 var game_scene: PackedScene = load("res://src/game.tscn")
 
 func _on_normal_mode_button_pressed():
-	GlobalEnvironment.bounciness = 0.0
+	GlobalEnvironment.generated_levels = false
 	get_tree().change_scene_to_packed(game_scene)
 
-
-func _on_bouncy_mode_button_pressed():
-	GlobalEnvironment.bounciness = 1.0
+func _on_generated_mode_button_pressed():
+	GlobalEnvironment.generated_levels = true
 	get_tree().change_scene_to_packed(game_scene)
